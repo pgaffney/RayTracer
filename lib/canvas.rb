3a8bedbd5dbf9626b03055fe0eaeb8c19e906ceb
Paul Gaffney <pgaffney2000@gmail.com>
2019-01-15 08:21:44 -0500

    PPM refactforing and comments

--- lib/canvas.rb
+++ lib/canvas.rb
@@ -12,10 +12,20 @@ class Canvas
     @pixels[row][column]
   end
 
+  # create output in PPM format
+  # fixed header:
+  #  P3
+  #  width, height
+  #  Maximun color value
+  # detail lines:
+  #  in lines of length no more than MAXPPMLINEs,
+  #  scaled red green and blue values output as space delimited integers
+  #  (scaling is from MINCOLOR to MAXCOLOR, linear rounded integers, negatives clamped to minimum
+  #  over max clamped to maximum)
   def to_ppm
     puts "P3"
     puts "#{width} #{height}"
-    puts "255"
+    puts "#{MAXCOLOR}"
 
     pixels.each do |row|
       charcount = 0
@@ -25,13 +35,13 @@ class Canvas
           scaled_color = rowcol.to_a[color] * MAXCOLOR
           scaled_color = MINCOLOR if scaled_color <= MINCOLOR
           scaled_color = MAXCOLOR if scaled_color >= MAXCOLOR
-          if charcount + scaled_color.round.to_s.length < MAXPPMLINE
-            print "#{scaled_color.round} "
-            charcount += scaled_color.round.to_s.length + 1
+          if charcount + scaled_color.round.to_s.length < MAXPPMLINE # if we are still less than a line
+            print "#{scaled_color.round} "                       # print the color value and its space delim
+            charcount += scaled_color.round.to_s.length + 1          # increment the character count
           else
-            print "\n"
-            print "#{scaled_color.round} "
-            charcount = scaled_color.round.to_s.length + 1
+            print "\n"                                           # since we have gone over the line limit
+            print "#{scaled_color.round} "                       # print a newline and the color value + delim
+            charcount = scaled_color.round.to_s.length + 1           # reset the character count
           end
         end
       end
